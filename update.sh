3caa01a2370db846ffadfbf83c8de37b85985542
jun yamada <jun.16@mac.com>
2025-06-11 20:37:11 +0900

    Update

--- update.sh
+++ update.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 #
 if [[ -f ./Brewfile ]]; then
-	rm ./Brewfile
+  rm ./Brewfile
 fi
 
 brew bundle dump
+
+# mas list | awk '{
+#   id = $1;
+#   sub(/^[0-9]+[ \t]+/, "");       # Remove the leading ID and whitespace
+#   sub(/[ \t]+\([0-9.]+\)$/, "");  # Remove the version in parentheses at the end
+#   print "mas \"" $0 "\", id: " id;
+# }' >>Brewfile
